f12c1016bd6d562427df7cd2fdb8c74746a3c959
Anoop M <noop2016@gmail.com>
2018-02-07 21:04:22 +0000

    changed name

--- web_app/app_server/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Initialize Blueprint views"""
-from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/app_server/v1")
-
-from app_server.v1.views.view1 import *
-from app_server.v1.views.view2 import *
-from app_server.v1.views.view3 import *
